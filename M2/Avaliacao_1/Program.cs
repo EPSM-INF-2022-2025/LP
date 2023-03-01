c2344bed3dfc78570047eaeb0f3e78beec3e94c7
Rafael Silva <rafsilva19@gmail.com>
2023-03-01 10:46:38 +0000

    M2 Avaliacao

---
+++ M2/Avaliacao_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/*
+ Escreva um programa que solicite ao usuário um 
+número inteiro e exiba uma mensagem informando se o 
+número é positivo, negativo ou zero.
+ */
+
+// Declarar variaveis
+int numero;
+
+// Perguntar ao utilizador
+Console.WriteLine("Qual o numero?");
+
+// Ler e converter valor do utilizador
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0) {
+    Console.WriteLine("O numero é positivo");
+} else if (numero < 0)
+{
+    Console.WriteLine("O numero é negativo");
+} else if (numero == 0)
+{
+    Console.WriteLine("O numero é zero");
+}
---
+++ M2/Avaliacao_2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Desenvolva um programa que pergunta ao utilizador o
+//tempo da corrida de dois atletas (horas, minutos e
+//segundos) e no final indique qual dos atletas venceu.
+
+// Declara variaveis
+int h1,m1,s1,h2,m2,s2;
+int tempo1, tempo2;
+
+Console.WriteLine("Quantas horas? (Atleta 1)");
+h1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantos minutos? (Atleta 1)");
+m1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantos segundos? (Atleta 1)");
+s1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantas horas? (Atleta 2)");
+h2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantos minutos? (Atleta 2)");
+m2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantos segundos? (Atleta 2)");
+s2 = Convert.ToInt32(Console.ReadLine());
+
+tempo1 = s1 + (m1 * 60) + (h1 * 60 * 60);
+tempo2 = s2 + (m2 * 60) + (h2 * 60 * 60);
+
+if (tempo1 < tempo2)
+{
+    Console.WriteLine("Ganhou o ATLETA 1");
+} else if (tempo2 < tempo1)
+{
+    Console.WriteLine("Ganhou o ATLETA 2");
+} else
+{
+    Console.WriteLine("EMPATE");
+}
+
+
