00dab1b7b3c719a3f8c2531271ddb549a3f46873
rafaelsilvaepsm <rafael.silva@epsm.pt>
2023-03-03 10:22:30 +0000

    Lancamento de Moedas

---
+++ M3/LancamentoMoedas/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int nrCaras = 0;
+int nrCoroas = 0;
+Random r = new Random();
+
+for (int i = 0; i < 20; i++)
+{
+    int resultado = r.Next(0, 2);
+
+    if (resultado == 0)
+    {
+        nrCaras++;
+    } else if (resultado == 1) {
+        nrCoroas++;
+    }
+}
+
+Console.WriteLine("Caras: " + nrCaras);
+Console.WriteLine("Coroas: " + nrCoroas);
