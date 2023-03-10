df103daa5fbc2db1535a5d9737edaa05dd47d594
rafaelsilvaepsm <rafael.silva@epsm.pt>
2023-03-10 10:20:07 +0000

    Fibonnaci

---
+++ M3/Fibonnaci/Program.cs
@@ -0,0 +1,19 @@
+﻿int sequencia;
+int numero1 = 0;
+int numero2 = 1;
+
+
+
+Console.WriteLine("Quantas sequencias queres fazer?");
+sequencia = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numero1);
+Console.WriteLine(numero2);
+
+for (int i = 0; i < sequencia - 2; i++)
+{
+
+    int temp = numero2;
+    numero2 = numero1 + numero2;
+    numero1 = temp;
+    Console.WriteLine(numero2);
+}
