97c4fc59d6ce76849a06290c97e78d6da591fa3b
rafaelsilvaepsm <rafael.silva@epsm.pt>
2023-04-26 10:48:56 +0100

    Changes

---
+++ M4/Banco/Program.cs
@@ -0,0 +1,50 @@
+﻿// Executar Sub-Programa
+MostrarMenuInicial();
+
+static void MostrarMenuInicial() {
+
+    string nomeTitular = "";
+    decimal saldoConta = 0;
+
+    // Declarar variável
+    bool continuarPrograma = true;
+
+    while (continuarPrograma)
+    {
+        // Mostrar menu
+        Console.WriteLine("1 - Criar Conta");
+        Console.WriteLine("2 - Depositar");
+        Console.WriteLine("3 - Levantar");
+        Console.WriteLine("0 - Sair");
+
+        // Guardar opção escolhida pelo utilizador
+        int opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcaoEscolhida)
+        {
+            case 1:
+                CriarConta(ref nomeTitular, ref saldoConta);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 0:
+                continuarPrograma = false;
+                break;
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+        }
+    }
+
+}
+
+static void CriarConta(ref string nomeTitular, ref decimal saldoConta)
+{
+    Console.WriteLine("Qual o nome?");
+    nomeTitular = Console.ReadLine();
+
+    Console.WriteLine("Qual o saldo?");
+    saldoConta = Convert.ToDecimal(Console.ReadLine());
+}
