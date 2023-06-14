b31c0b24f21787d23f3d768177ea616e485a3fae
rafaelsilvaepsm <rafael.silva@epsm.pt>
2023-06-14 08:37:15 +0100

    Arrays

---
+++ M4/ArrayBidimensional/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int[,] alunos = new int[3,2];
+
+        for (int i = 0; i < alunos.Length; i++)
+        {
+            Console.WriteLine("Qual o  aluno?");
+            alunos[i, 0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual a idade?");
+            alunos[i,1] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+
+    }
+}
---
+++ M4/ArraySimples/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] idades = new int[5];
+
+        for (int i = 0; i < idades.Length; i++)
+        {
+            Console.WriteLine("Qual a sua idade?");
+            idades[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        int soma = 0;
+        decimal media;
+        for (int i = 0; i < idades.Length; i++)
+        {
+            soma = soma + idades[i];
+        }
+
+        media = soma / idades.Length;
+
+        Console.WriteLine("Média = " +  media);
+
+    }
+}
