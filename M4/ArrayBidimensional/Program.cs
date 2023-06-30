fe13e29158d9e29d7c25409a8ddd84b36eca725d
rafaelsilvaepsm <rafael.silva@epsm.pt>
2023-06-30 09:26:02 +0100

    M4

--- M4/ArrayBidimensional/Program.cs
+++ M4/ArrayBidimensional/Program.cs
@@ -3,9 +3,9 @@
     private static void Main(string[] args)
     {
 
-        int[,] alunos = new int[3,2];
+        int[,] alunos = new int[4,2];
 
-        for (int i = 0; i < alunos.Length; i++)
+        for (int i = 0; i < alunos.GetLength(0); i++)
         {
             Console.WriteLine("Qual o  aluno?");
             alunos[i, 0] = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +14,21 @@
             alunos[i,1] = Convert.ToInt32(Console.ReadLine());
         }
 
+        int maiorIdade = 0;
+        int numeroAluno = 0;
+
+        for (int i = 0; i < alunos.GetLength(0); i++)
+        {
+            int idade = alunos[i, 1];
+
+            if (idade > maiorIdade)
+            {
+                maiorIdade = idade;
+                numeroAluno = alunos[i, 0];
+            }
+        }
+
+        Console.WriteLine("O Aluno tem " + maiorIdade + " e o número " + numeroAluno);
         
 
     }
---
+++ M7/Avaliacao/Program.cs
@@ -0,0 +1,144 @@
+﻿internal class Program
+{
+
+    internal static string[] titulos = new string[3];
+    internal static string[] autores = new string[3];
+    internal static double[] precos = new double[3];
+
+    private static void Main(string[] args)
+    {
+        MostrarMenu();
+    }
+
+    private static void MostrarMenu()
+    {
+        Console.WriteLine("1 - Registar Livros");
+        Console.WriteLine("2 - Mostrar mais caro");
+        Console.WriteLine("3 - Mostrar mais barato");
+        Console.WriteLine("4 - Mostrar média");
+        Console.WriteLine("5 - Mostrar livros de autor");
+        Console.WriteLine("0 - Sair");
+
+        int opcao = Convert.ToInt32(Console.ReadLine());
+        bool continuar = true;
+
+        while (continuar)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    RegistarLivros();
+                    break;
+                case 2:
+                    MostrarMaisCaro();
+                    break;
+                case 3:
+                    MostrarMaisBarato();
+                    break;
+                case 4:
+                    MostrarMedia();
+                    break;
+                case 5:
+                    MostrarLivrosAutor();
+                    break;
+                case 0:
+                    continuar = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        
+    }
+
+    private static void MostrarLivrosAutor()
+    {
+        Console.WriteLine("Qual o autor?");
+        string autor = Console.ReadLine();
+
+        for (int i = 0; i < autores.Length; i++)
+        {
+            if (autores[i] == autor) {
+                Console.WriteLine("Titulo = " + titulos[i]);
+                Console.WriteLine("Autor = " + autores[i]);
+                Console.WriteLine("Preço = " + precos[i]);
+            }
+        }
+    }
+
+    private static void MostrarMedia()
+    {
+        double media;
+        double soma = 0;
+
+        for (int i = 0; i < precos.Length; i++)
+        {
+            soma = soma + precos[i];
+        }
+
+        media = soma / precos.Length;
+
+        Console.WriteLine("Média = " + media);
+    }
+
+    private static void MostrarMaisBarato()
+    {
+        string tituloMaisBarato = "";
+        string autorMaisBarato = "";
+        double precoMaisBarato = double.MaxValue;
+
+        for (int i = 0; i < precos.Length; i++)
+        {
+            if (precos[i] < precoMaisBarato)
+            {
+                precoMaisBarato = precos[i];
+                tituloMaisBarato = titulos[i];
+                autorMaisBarato = autores[i];
+            }
+        }
+
+        Console.WriteLine("Titulo = " + tituloMaisBarato);
+        Console.WriteLine("Autor = " + autorMaisBarato);
+        Console.WriteLine("Preço = " + precoMaisBarato);
+    }
+
+    private static void MostrarMaisCaro()
+    {
+        string tituloMaisCaro = "";
+        string autorMaisCaro = "";
+        double precoMaisCaro = 0;
+
+        for (int i = 0; i < precos.Length; i++)
+        {
+
+            if (precos[i] > precoMaisCaro)
+            {
+                precoMaisCaro = precos[i];
+                tituloMaisCaro = titulos[i];
+                autorMaisCaro = autores[i];
+            }
+
+        }
+
+        Console.WriteLine("Titulo = " + tituloMaisCaro);
+        Console.WriteLine("Autor = " + autorMaisCaro);
+        Console.WriteLine("Preço = " + precoMaisCaro);
+    }
+
+    private static void RegistarLivros()
+    {
+        for (int i = 0; i < titulos.Length; i++)
+        {
+            Console.WriteLine("Qual o titulo?");
+            titulos[i] = Console.ReadLine();
+
+            Console.WriteLine("Qual o autor?");
+            autores[i] = Console.ReadLine();
+
+            Console.WriteLine("Qual o preço?");
+            precos[i] = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
